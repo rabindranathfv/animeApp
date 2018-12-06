4fa8aee58457f5cdb615f4578ad035eceb01c35c
Rabindranath Ferreira <ferreiralox@gmail.com>
2018-12-06 19:37:21 -0300

    add Auth0 to angular.json and install npm about it

---
+++ src/app/auth0/enviroment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    auth0: {
+      domain: 'authanimeapp.auth0.com',
+      clientId: 'ErHiZZoBLd0sdXB1HRICI7Sjz773nQi1',
+      callbackURL: 'http://localhost:4200/login'
+    }
+  };
